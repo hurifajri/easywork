2f589aa60464c33088154f46b883d38c4e7ec03d
Muhamad Fajri <hurifajri@gmail.com>
2020-09-12 09:16:56 +0700

    Build main with content

--- src/containers/app.js
+++ src/containers/app.js
@@ -10,9 +10,11 @@ import {
   Navbar,
   Row,
 } from 'react-bootstrap';
+import firstContent from '../assets/images/content-001.png';
 import logo from '../assets/images/logo.png';
 import React from 'react';
 import SearchIcon from '../components/icons/search';
+import secondContent from '../assets/images/content-002.png';
 
 export default () => {
   const links = [
@@ -67,7 +69,53 @@ export default () => {
       </Row>
       {/* Main */}
       <Row className="main">
-        <Col>This is main</Col>
+        <Col>
+          <Container>
+            {/* Content */}
+            <Row className="content">
+              <Col xs={12} md={7} className="image">
+                <img src={firstContent} alt="First content" />
+              </Col>
+              <Col xs={12} md={5} className="text">
+                <Container>
+                  <h2 className="title">
+                    We can give you our best user experience to your system
+                  </h2>
+                  <p className="description">
+                    Lorem ipsum is simply dummy text of the printing and
+                    typesetting industry. Lorem ipsum has been the industry's
+                    standard dummy text ever since the 1500s, when an unknown
+                    printer took a galley of type and scrambled it to make a
+                    type specimen book. It has survived not only five centuries,
+                    but also the leap into electronic typesetting, remaining
+                    essentially unchanged.
+                  </p>
+                </Container>
+              </Col>
+            </Row>
+            <Row className="content">
+              <Col xs={12} md={5} className="text">
+                <Container>
+                  <h2 className="title">
+                    Easy access. Whenever, wherever you want
+                  </h2>
+                  <p className="description">
+                    Lorem ipsum is simply dummy text of the printing and
+                    typesetting industry. Lorem ipsum has been the industry's
+                    standard dummy text ever since the 1500s, when an unknown
+                    printer took a galley of type and scrambled it to make a
+                    type specimen book. It has survived not only five centuries,
+                    but also the leap into electronic typesetting, remaining
+                    essentially unchanged.
+                  </p>
+                </Container>
+              </Col>
+              <Col xs={12} md={7} className="image">
+                <img src={secondContent} alt="Second content" />
+              </Col>
+            </Row>
+          </Container>
+        </Col>
       </Row>
       {/* Footer */}
       <Row className="footer">
