b5ecf9f340b5c65f9a44b15938a070917a516665
Muhamad Fajri <hurifajri@gmail.com>
2020-09-12 17:13:26 +0700

    Build main with testimonial

--- src/containers/app.js
+++ src/containers/app.js
@@ -1,6 +1,10 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
 import './app.css';
 import {
+  Button,
+  Card,
   Col,
   Container,
   Form,
@@ -10,13 +14,21 @@ import {
   Navbar,
   Row,
 } from 'react-bootstrap';
-import firstContent from '../assets/images/content-001.png';
+import React, { useRef } from 'react';
+import ArrowIcon from '../components/icons/arrow';
+import contentOne from '../assets/images/content-001.png';
+import contentTwo from '../assets/images/content-002.png';
 import logo from '../assets/images/logo.png';
-import React from 'react';
+import LoveIcon from '../components/icons/love';
+import personOne from '../assets/images/person-001.jpg';
+import personThree from '../assets/images/person-002.jpg';
+import personTwo from '../assets/images/person-003.jpg';
 import SearchIcon from '../components/icons/search';
-import secondContent from '../assets/images/content-002.png';
+import Slider from 'react-slick';
 
 export default () => {
+  const refContainer = useRef(null);
+
   const links = [
     { id: 1, title: 'Home', href: '#home' },
     { id: 2, title: 'About Us', href: '#about-us' },
@@ -24,6 +36,67 @@ export default () => {
     { id: 4, title: 'Pricing', href: '#pricing' },
     { id: 5, title: 'Careers', href: '#careers' },
   ];
+
+  const testimonials = [
+    {
+      id: 1,
+      name: 'David Champion',
+      role: 'CEO of iCloud',
+      img: personOne,
+      message:
+        'System is excelent. It has made my system user experience to become one of the easiest!',
+    },
+    {
+      id: 2,
+      name: 'David Frank van Hord',
+      role: 'CEO of Marks.co',
+      img: personTwo,
+      message:
+        'I just wanted to share a quick note and let you know that you guys do a really good job.',
+    },
+    {
+      id: 3,
+      name: 'Lucas Bond',
+      role: 'BOD of Skyscanner',
+      img: personThree,
+      message:
+        "Now it's almost like having a designer right. I just choose the page, make the change.",
+    },
+    {
+      id: 4,
+      name: 'Lucas Bond',
+      role: 'BOD of Skyscanner',
+      img: personThree,
+      message:
+        "Now it's almost like having a designer right. I just choose the page, make the change.",
+    },
+  ];
+
+  const settings = {
+    dots: true,
+    // infinite: true,
+    // autoplay: true,
+    speed: 500,
+    arrows: false,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+        },
+      },
+      {
+        breakpoint: 576,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
+  };
   return (
     <Container fluid>
       {/* Header */}
@@ -50,7 +123,11 @@ export default () => {
                 </Nav>
                 <Form inline className="search-form">
                   <FormControl type="text" className="search-input" />
-                  <SearchIcon size={15} color="white" className="search-icon" />
+                  <SearchIcon
+                    size={15}
+                    color="var(--white)"
+                    className="search-icon"
+                  />
                 </Form>
               </Navbar.Collapse>
             </Container>
@@ -70,56 +147,114 @@ export default () => {
       {/* Main */}
       <Row className="main">
         <Col>
-          <Container>
-            {/* Content */}
-            <Row className="content">
-              <Col xs={12} md={7} className="image">
-                <img src={firstContent} alt="First content" />
-              </Col>
-              <Col xs={12} md={5} className="text">
-                <Container>
+          {/* Content */}
+          <Row className="content">
+            <Col xs={12} md={6} lg={7} className="image">
+              <Container>
+                <img src={contentOne} alt="First content" />
+              </Container>
+            </Col>
+            <Col xs={12} md={6} lg={5} className="text">
+              <Container>
+                <h2 className="title">
+                  We can give you our best user experience to your system
+                </h2>
+                <p className="description">
+                  Lorem ipsum is simply dummy text of the printing and
+                  typesetting industry. Lorem ipsum has been the industry's
+                  standard dummy text ever since the 1500s, when an unknown
+                  printer took a galley of type and scrambled it to make a type
+                  specimen book. It has survived not only five centuries, but
+                  also the leap into electronic typesetting, remaining
+                  essentially unchanged.
+                </p>
+              </Container>
+            </Col>
+          </Row>
+          <Row className="content">
+            <Col xs={12} md={6} lg={5} className="text">
+              <Container>
+                <h2 className="title">
+                  Easy access. Whenever, wherever you want
+                </h2>
+                <p className="description">
+                  Lorem ipsum is simply dummy text of the printing and
+                  typesetting industry. Lorem ipsum has been the industry's
+                  standard dummy text ever since the 1500s, when an unknown
+                  printer took a galley of type and scrambled it to make a type
+                  specimen book. It has survived not only five centuries, but
+                  also the leap into electronic typesetting, remaining
+                  essentially unchanged.
+                </p>
+              </Container>
+            </Col>
+            <Col xs={12} md={6} lg={7} className="image">
+              <Container>
+                <img src={contentTwo} alt="Second content" />
+              </Container>
+            </Col>
+          </Row>
+          {/* Testimonial */}
+          <Row className="testimonial">
+            <Col>
+              <Container>
+                <div className="testimonial-heading">
+                  <span className="icon-wrapper">
+                    <LoveIcon
+                      size={13}
+                      color="var(--white)"
+                      className="love-icon"
+                    />
+                  </span>
                   <h2 className="title">
-                    We can give you our best user experience to your system
+                    What other people say about our service
                   </h2>
-                  <p className="description">
-                    Lorem ipsum is simply dummy text of the printing and
-                    typesetting industry. Lorem ipsum has been the industry's
-                    standard dummy text ever since the 1500s, when an unknown
-                    printer took a galley of type and scrambled it to make a
-                    type specimen book. It has survived not only five centuries,
-                    but also the leap into electronic typesetting, remaining
-                    essentially unchanged.
-                  </p>
-                </Container>
-              </Col>
-            </Row>
-            <Row className="content">
-              <Col xs={12} md={5} className="text">
-                <Container>
-                  <h2 className="title">
-                    Easy access. Whenever, wherever you want
-                  </h2>
-                  <p className="description">
-                    Lorem ipsum is simply dummy text of the printing and
-                    typesetting industry. Lorem ipsum has been the industry's
-                    standard dummy text ever since the 1500s, when an unknown
-                    printer took a galley of type and scrambled it to make a
-                    type specimen book. It has survived not only five centuries,
-                    but also the leap into electronic typesetting, remaining
-                    essentially unchanged.
-                  </p>
-                </Container>
-              </Col>
-              <Col xs={12} md={7} className="image">
-                <img src={secondContent} alt="Second content" />
-              </Col>
-            </Row>
-          </Container>
+                </div>
+                <Button
+                  className="next-button"
+                  onClick={() => refContainer.current.slickNext()}
+                >
+                  <ArrowIcon
+                    size={15}
+                    color="var(--purple)"
+                    className="arrow-icon"
+                  />
+                </Button>
+              </Container>
+            </Col>
+          </Row>
+          {/* Testimonial Slider */}
+          <Row className="testimonial-slider">
+            <Col>
+              <Container fluid>
+                <Slider ref={refContainer} {...settings}>
+                  {testimonials.map(person => (
+                    <Card key={person.id}>
+                      <div className="card-detail">
+                        <Card.Img src={person.img} />
+                        <div>
+                          <Card.Title>{person.name}</Card.Title>
+                          <Card.Subtitle className="text-muted">
+                            {person.role}
+                          </Card.Subtitle>
+                        </div>
+                      </div>
+                      <Card.Body>
+                        <Card.Text>"{person.message}"</Card.Text>
+                      </Card.Body>
+                    </Card>
+                  ))}
+                </Slider>
+              </Container>
+            </Col>
+          </Row>
         </Col>
       </Row>
       {/* Footer */}
       <Row className="footer">
-        <Col>This is footer</Col>
+        <Col>
+          <Container>This is footer</Container>
+        </Col>
       </Row>
     </Container>
   );
